817694c0fe5671b90b5c4b0f5ecf58f23612acd5
hotoniel.silva <hotoniel.silva@coris.com.br>
2024-04-30 08:07:15 -0300

    novas abas

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -12,22 +12,23 @@ import AccountCircle from "@mui/icons-material/AccountCircle"
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import DescriptionIcon from '@mui/icons-material/Description';
+import HomeIcon from '@mui/icons-material/Home';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import CurrencyExchangeIcon from '@mui/icons-material/CurrencyExchange';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Footer from './Footer'
-import DescriptionIcon from '@mui/icons-material/Description';
-import HomeIcon from '@mui/icons-material/Home';
+
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Home from '../pages/Home'
 import NotasFiscais from '../pages/NotasFiscais'
 import Agentes from '../pages/Agentes'
 import ContasPagar from '../pages/ContasPagar'
 
-
 const drawerWidth = 240;
-
 
 const openedMixin = (theme) => ({
   width: drawerWidth,
@@ -108,124 +109,175 @@ export default function SideBar() {
   };
 
   return (
-    <>
-      <BrowserRouter>
-        <Box sx={{ display: 'flex' }}>
-          <CssBaseline />
-          <AppBar position="fixed" open={open}>
-            <Toolbar>
-              <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                onClick={handleDrawerOpen}
-                edge="start"
+    <BrowserRouter>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppBar position="fixed" open={open}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              sx={{
+                marginRight: 5,
+                ...(open && { display: 'none' }),
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap component="div">
+              Coris - Financeiro
+            </Typography>
+
+            <Box sx={{ flexGrow: 1 }}></Box>
+
+            <Stack direction="row" spacing={2}>
+              <IconButton color="inherit">
+                <Badge badgeContent={0} color="error">
+                  <NotificationsIcon></NotificationsIcon>
+                </Badge>
+              </IconButton>
+
+              <IconButton color="inherit">
+                <AccountCircle></AccountCircle>
+              </IconButton>
+            </Stack>
+          </Toolbar>
+        </AppBar>
+
+        <Drawer variant="permanent" open={open}>
+          <DrawerHeader>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+            </IconButton>
+          </DrawerHeader>
+          <Divider />
+          <List>
+            <ListItem
+              key={"Home"}
+              disablePadding
+              sx={{ display: 'block' }}
+            >
+              <ListItemButton
                 sx={{
-                  marginRight: 5,
-                  ...(open && { display: 'none' }),
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
                 }}
-              >
-                <MenuIcon />
-              </IconButton>
-              <Typography variant="h6" noWrap component="div">
-                Coris - Financeiro
-              </Typography>
-
-              <Box sx={{ flexGrow: 1 }}></Box>
-
-              <Stack direction="row" spacing={2}>
-                <IconButton color="inherit">
-                  <Badge badgeContent={0} color="error">
-                    <NotificationsIcon></NotificationsIcon>
-                  </Badge>
-                </IconButton>
-
-                <IconButton color="inherit">
-                  <AccountCircle></AccountCircle>
-                </IconButton>
-              </Stack>
-            </Toolbar>
-          </AppBar>
-
-          <Drawer variant="permanent" open={open}>
-            <DrawerHeader>
-              <IconButton onClick={handleDrawerClose}>
-                {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-              </IconButton>
-            </DrawerHeader>
-            <Divider />
-            <List>
-              <ListItem
-                key={"Home"}
-                disablePadding
-                sx={{ display: 'block' }}
                 component={Link} to="/"
               >
-                <ListItemButton
+                <ListItemIcon
                   sx={{
-                    minHeight: 48,
-                    justifyContent: open ? 'initial' : 'center',
-                    px: 2.5,
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
                   }}
-
                 >
-                  <ListItemIcon
-                    sx={{
-                      minWidth: 0,
-                      mr: open ? 3 : 'auto',
-                      justifyContent: 'center',
-                    }}
-                  >
-                    <HomeIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="Home" sx={{ opacity: open ? 1 : 0 }} />
-                </ListItemButton>
-              </ListItem>
+                  <HomeIcon />
+                </ListItemIcon>
+                <ListItemText primary="Home" sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+          </List>
+          <List>
+            <ListItem
+              key={"Agentes"}
+              disablePadding
+              sx={{ display: 'block' }}
+            >
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+                component={Link} to="/Agentes"
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                  <PeopleAltIcon />
+                </ListItemIcon>
+                <ListItemText primary="Agentes" sx={{ opacity: open ? 1 : 0 }} />
 
-            </List>
-            <List>
-              <ListItem
-                key={"Notas Fiscais"}
-                disablePadding
-                sx={{ display: 'block' }}
+              </ListItemButton>
+            </ListItem>
+          </List>
+          <List>
+            <ListItem
+              key={"Notas Fiscais"}
+              disablePadding
+              sx={{ display: 'block' }}
+            >
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                }}
                 component={Link} to="/NotasFiscais"
               >
-                <ListItemButton
+                <ListItemIcon
                   sx={{
-                    minHeight: 48,
-                    justifyContent: open ? 'initial' : 'center',
-                    px: 2.5,
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
                   }}
-                  
                 >
-                  <ListItemIcon
-                    sx={{
-                      minWidth: 0,
-                      mr: open ? 3 : 'auto',
-                      justifyContent: 'center',
-                    }}
-                  >
-                    <DescriptionIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="Notas Fiscais" sx={{ opacity: open ? 1 : 0 }} />
-                  
-                </ListItemButton>
-              </ListItem>
-            </List>
-            <Divider />
-          </Drawer>
+                  <DescriptionIcon />
+                </ListItemIcon>
+                <ListItemText primary="Notas Fiscais" sx={{ opacity: open ? 1 : 0 }} />
 
-          <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-            <DrawerHeader />
-            <Routes>
-              <Route path="/" exact element={<Home />} />
-              <Route path="/Agentes" exact element={<Agentes />} />
-              <Route path="/ContasPagar" exact element={<ContasPagar />} />
-              <Route path="/NotasFiscais" exact element={<NotasFiscais />} />
-            </Routes>
-          </Box>
+              </ListItemButton>
+            </ListItem>
+          </List>
+          <List>
+            <ListItem
+              key={"ContasPagar"}
+              disablePadding
+              sx={{ display: 'block' }}
+            >
+              <ListItemButton
+                sx={{
+                  minHeight: 48,
+                  justifyContent: open ? 'initial' : 'center',
+                  px: 2.5,
+                }}
+                component={Link} to="/ContasPagar"
+              >
+                <ListItemIcon
+                  sx={{
+                    minWidth: 0,
+                    mr: open ? 3 : 'auto',
+                    justifyContent: 'center',
+                  }}
+                >
+                  <CurrencyExchangeIcon />
+                </ListItemIcon>
+                <ListItemText primary="Reembolsos" sx={{ opacity: open ? 1 : 0 }} />
+              </ListItemButton>
+            </ListItem>
+          </List>
+          <Divider />
+        </Drawer>
+
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          <DrawerHeader />
+          <Routes>
+            <Route path="/" exact element={<Home />} />
+            <Route path="/Agentes" exact element={<Agentes />} />
+            <Route path="/ContasPagar" exact element={<ContasPagar />} />
+            <Route path="/NotasFiscais" exact element={<NotasFiscais />} />
+            <Route path="/ContasPagar" exact element={<ContasPagar />} />
+          </Routes>
         </Box>
-        <Footer />
-      </BrowserRouter >
-    </>
+      </Box>
+      <Footer />
+    </BrowserRouter >
   );
 }
